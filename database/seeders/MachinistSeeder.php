5dd53ce259c603358f693f940f81dd232d5b8100
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-28 16:40:09 +0100

    fixing MachinistSeeder.php

--- database/seeders/MachinistSeeder.php
+++ database/seeders/MachinistSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Machinist;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -14,6 +15,13 @@ class MachinistSeeder extends Seeder
     public function run(Faker $faker): void
     {
         //
-
+        for ( $i=0; $i < 20; $i++ ) {
+            $newMachinist = new Machinist();
+            $newMachinist->first_name = $faker->firstName();
+            $newMachinist->last_name = $faker->lastName();
+            $newMachinist->date_of_birth = $faker->dateTimeBetween("-60 years", "-25 years");
+            $newMachinist->country = $faker->country();
+            $newMachinist->save();
+        }
     }
 }
