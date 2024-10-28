bb95b2ebe54847ac39095a75ba33eb0dd2d10dfa
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-28 09:54:02 +0100

    added nullable in migrations

--- database/migrations/2024_10_25_133007_create_trains_table.php
+++ database/migrations/2024_10_25_133007_create_trains_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string("Azienda", 100)->unique;
-            $table->date("Data_di_partenza");
-            $table->string("Stazione_di_partenza", 150);
-            $table->string("Stazione_di_arrivo", 150);
-            $table->time("Orario_di_partenza");
-            $table->time("Orario_di_arrivo");
-            $table->string("Codice_treno")->unique;
+            $table->string("Azienda", 100)->nullable(false);
+            $table->date("Data_di_partenza")->nullable(false);
+            $table->string("Stazione_di_partenza", 150)->nullable(false);
+            $table->string("Stazione_di_arrivo", 150)->nullable(false);
+            $table->time("Orario_di_partenza")->nullable(false);
+            $table->time("Orario_di_arrivo")->nullable(false);
+            $table->string("Codice_treno")->unique->nullable(false);
             $table->tinyInteger("Numero_carrozze")->unsigned;
-            $table->boolean("In_orario")->default(true);
-            $table->boolean("Cancellato")->default(false);
+            $table->boolean("In_orario")->default(true)->nullable(false);
+            $table->boolean("Cancellato")->default(false)->nullable(false);
             $table->timestamps();
         });
     }
