8ed396807099b284c1685c9beac7c07b670a6dae
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-28 12:30:46 +0100

    removed faker from  migration passengers

--- database/migrations/2024_10_28_111835_create_passengers_table.php
+++ database/migrations/2024_10_28_111835_create_passengers_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Faker\Generator as Faker;
 
 return new class extends Migration
 {
@@ -14,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('passengers', function (Blueprint $table) {
             $table->id();
-            $table->
             $table->timestamps();
         });
     }
