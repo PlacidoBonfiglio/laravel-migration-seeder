37fbc4385500a58ecdc7e9d0ce8f2b09cfaf7976
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-28 15:57:13 +0100

    end

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,4 +6,8 @@
     <a href="/trains">
         <h3 class="text-center pb-5">Cliccami per vedere solo i treni che partono il 2024/10/25</h3>
     </a>
+
+    <a href="/passengers">
+        <h3 class="text-center pb-5">Cliccami per vedere i passeggeri</h3>
+    </a>
 @endsection
--- resources/views/passengers/index.blade.php
+++ resources/views/passengers/index.blade.php
@@ -8,7 +8,14 @@
     <div class="container">
         <div class="row row-cols-5 justify-content-center gap-5">
 
-
+            @foreach ( $passengers as $passenger )
+                <div class="col text-center">
+                    <p>ID: <span class="fw-bold">{{$passenger->id}}</span></p>
+                    <p>Nome: <span class="fw-bold">{{$passenger->first_name}}</span></p>
+                    <p>Cognome: <span class="fw-bold">{{$passenger->last_name}}</span></p>
+                    <p>Data di nascita: <span class="fw-bold">{{$passenger->date_of_birth}}</span></p>
+                </div>
+            @endforeach
 
         </div>
     </div>
